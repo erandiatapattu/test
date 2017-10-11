3112605ab3c304f186758e482464953fffe853e2
Ashantha Lahiru <ashantha.lahiru@gmail.com>
2017-10-11 23:29:49 +0530

    Initial Project

---
+++ app/src/main/java/com/example/ashantha/connectme/Advertise.java
@@ -0,0 +1,16 @@
+package com.example.ashantha.connectme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Advertise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertise);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/ashantha/connectme/Login.java
@@ -0,0 +1,51 @@
+package com.example.ashantha.connectme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    EditText pNumber;
+    Button save;
+
+    SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        prefs = this.getSharedPreferences("appdata", Context.MODE_PRIVATE);
+
+        if(prefs.getString ("pnum",null) != null){
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+        }
+
+            save = (Button) findViewById(R.id.btnSave);
+            pNumber = (EditText) findViewById(R.id.pNumber);
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                saveNumber();
+            }
+        });
+    }
+
+    public void saveNumber()
+    {
+        String phone = pNumber.getText().toString();
+        prefs.edit().putString("pnum", phone).apply();
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+    }
+}
